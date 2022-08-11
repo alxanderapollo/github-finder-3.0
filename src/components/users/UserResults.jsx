c07f37b2c7a55ba302613f5cba5834ef90185cbf
Alex <alexsalazarny@gmail.com>
2022-08-11 12:21:47 -0400

    Set up user results and user items to display when fetching data

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 //request to make the users loads as soon as the app loads
 import { useEffect, useState } from "react";
+import UserItem from "./UserItem";
 
 import Spinner from "../layout/Spinner";
 function UserResults() {
@@ -31,7 +32,7 @@ function UserResults() {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
