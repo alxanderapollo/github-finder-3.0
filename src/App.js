5f537b7923b524aa253b32005561218dc2fdd575
Alex <alexsalazarny@gmail.com>
2022-08-12 18:14:09 -0400

    Added Alert context, and provider

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import NotFound from "./components/pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        {/* take the full length of the screen vertically */}
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              {/* catch all if a page doesnt exist user vists here */}
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          {/* take the full length of the screen vertically */}
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                {/* catch all if a page doesnt exist user vists here */}
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
