cc9dc2b90b63246f37d8972a981573673c1728e7
Alex <alexsalazarny@gmail.com>
2022-08-11 21:27:36 -0400

    adding search form functionality

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import UserResults from "../users/UserResults";
+import UserSearch from "../users/UserSearch";
 function Home() {
   return (
     <div>
       {/* SEarch component will go here */}
+      <UserSearch />
       <UserResults />
     </div>
   );
