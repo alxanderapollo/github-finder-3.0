f21a260fd604df38de251c8c30ee1f49d1388660
Alex <alexsalazarny@gmail.com>
2022-08-17 08:51:05 -0400

    Updated file for broken build

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from "react";
-import githubReducer from "./githubReducer";
+import githubReducer from "./GithubReducer";
 
 //create our context
 const GithubContext = createContext();
